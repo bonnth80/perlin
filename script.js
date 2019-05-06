35aae320ba28058c4b7469e949bd38a7eb8cd3a6
Tom Bonner <zerosavant@yahoo.com>
2019-05-05 19:07:33 -0500

    - style adjustments - logical layer member ordering

--- script.js
+++ script.js
@@ -22,50 +22,46 @@ var btnToggle4 = document.getElementById("btnToggle4");
 // layer data
 var layers = [
 	{
-		id: "layer1",
-		color: "#AFAFEF",
-		interval: 8,
-		dotSize: 5,
-		min: 20,
+		set: [],
 		max: 200,
-		set: [],
-		calcLayer: true
+		min: 20,
+		interval: 8,
+		calcLayer: true,
+		color: "#AFAFEF",
+		dotSize: 5
 	},
 	{
-		id: "layer2",
-		color: "#99EE99",
-		interval: 4,
-		dotSize: 4,
-		min: 40,
+		set: [],
 		max: 160,
-		set: [],
-		calcLayer: true
+		min: 40,
+		interval: 4,
+		calcLayer: true,
+		color: "#99EE99",
+		dotSize: 4
 	},
 	{
-		id: "layer3",
-		color: "#FFCC66",
-		interval: 2,
-		dotSize: 3,
-		min: 60,
+		set: [],
 		max: 140,
-		set: [],
-		calcLayer: true
+		min: 60,
+		interval: 2,
+		calcLayer: true,
+		color: "#FFCC66",
+		dotSize: 3
 	},
 	{
-		id: "layer4",
-		color: "#FF9999",
-		interval: 1,
-		dotSize: 2,
-		min: 80,
-		max: 120,
 		set: [],
-		calcLayer: true
+		max: 120,
+		min: 80,
+		interval: 1,
+		calcLayer: true,
+		color: "#FF9999",
+		dotSize: 2,
 	}
 ];
 
 appController = {
-	renderLayers: [true,true,true,true],
-	calcLayers: [true,true,true,true],
+	renderLayers: [true, true, true, true],
+	calcLayers: [true, true, true, true],
 	totals: [],
 	perlin: [],
 	color: "#000",
@@ -107,7 +103,7 @@ btnToggle4.addEventListener("click", function(){
 //----------------------------------------
 
 function clearChart(){
-	canv.clearRect(0,0,800,200);
+	canv.clearRect(0, 0, 800, 200);
 	drawGuides();
 }
 
@@ -116,33 +112,33 @@ function drawGuides(){
 	canv.font = "9px Arial"
 	canv.strokeStyle = "#DDDDFF";
 
-	for (var i = 0; i <= 200; i+=20) {
-		canv.moveTo(0,i-.5);
-		canv.lineTo(800,i-.5);
+	for (var i = 0; i <= 200; i += 20) {
+		canv.moveTo(0, i - 0.5);
+		canv.lineTo(800, i - 0.5);
 		canv.stroke();
 	}
 
-	for (var i = 0; i <= 800; i+=20) {
-		canv.moveTo(i+.5,200);
-		canv.lineTo(i+.5,0);
+	for (var i = 0; i <= 800; i += 20) {
+		canv.moveTo(i + 0.5, 200);
+		canv.lineTo(i + 0.5, 0);
 		canv.stroke();
 	}
 
 	canv.beginPath()
 	canv.strokeStyle = "#000000";
 
-	for (var i = 0; i <= 200; i+=20) {
-		canv.moveTo(0,i-.5);
-		canv.lineTo(10,i-.5);
+	for (var i = 0; i <= 200; i += 20) {
+		canv.moveTo(0, i - 0.5);
+		canv.lineTo(10, i - 0.5);
 		canv.stroke();
-		canv.fillText(200-i,11,i+2)
+		canv.fillText(200 - i, 11, i+2)
 	}	
 
-	for (var i = 0; i <= CANV_WIDTH; i+=20) {
-		canv.moveTo(i+.5,200);
-		canv.lineTo(i+.5,190);
+	for (var i = 0; i <= CANV_WIDTH; i += 20) {
+		canv.moveTo(i + 0.5, 200);
+		canv.lineTo(i + 0.5, 190);
 		canv.stroke();
-		canv.fillText(i*(appController.numDataPoints/CANV_WIDTH),i+2,199)
+		canv.fillText(i * (appController.numDataPoints / CANV_WIDTH),i + 2, 199)
 	}
 }
 
@@ -168,7 +164,7 @@ function generateLayerData() {
 }
 
 function perlin(objectX) {
-	for (var i = 0; i <= appController.numDataPoints; i+=1) {
+	for (var i = 0; i <= appController.numDataPoints; i++) {
 		appController.totals[i] = 0;
 
 		objectX.forEach(function(e){
@@ -217,9 +213,9 @@ function perlinScale(totals){
 		if (e.calcLayer)
 			totalMaxVal += e.max;
 	})
-	console.log(totals);
+	
 	for (var i = 0; i <= appController.numDataPoints; i++){
-		scaledSet[i] = totals[i]/totalMaxVal*200;
+		scaledSet[i] = totals[i] / totalMaxVal * 200;
 	}
 	return scaledSet;
 }
@@ -235,16 +231,16 @@ function renderLayer(objectX) {
 	canv.moveTo(0, objectX.set[0]);
 
 	// draw line
-	for (var i = 0; i <= appController.numDataPoints; i+=objectX.interval) {
-		canv.lineTo(i*(CANV_WIDTH/appController.numDataPoints),200-objectX.set[i]);		
+	for (var i = 0; i <= appController.numDataPoints; i += objectX.interval) {
+		canv.lineTo(i*(CANV_WIDTH/appController.numDataPoints), 200 - objectX.set[i]);		
 	}
 	canv.stroke();
 	
 	// draw dot
 	canv.fillStyle = objectX.color;
-	for (var i = 0; i <= appController.numDataPoints; i+=objectX.interval) {
+	for (var i = 0; i <= appController.numDataPoints; i += objectX.interval) {
 		canv.beginPath();
-		canv.arc(i*(CANV_WIDTH/appController.numDataPoints), 200-objectX.set[i], objectX.dotSize, 2, 360);
+		canv.arc(i * (CANV_WIDTH / appController.numDataPoints), 200 - objectX.set[i], objectX.dotSize, 2, 360);
 		canv.fill();
 	}
 }
@@ -254,15 +250,15 @@ function renderTotal(objectY) {
 	canv.strokeStyle = appController.color;
 	canv.moveTo(0, objectY.perlin[0]);
 
-	for (var i = 0; i <= appController.numDataPoints; i+=1) {
-		canv.lineTo(i*(CANV_WIDTH/appController.numDataPoints),200-objectY.perlin[i]);		
+	for (var i = 0; i <= appController.numDataPoints; i += 1) {
+		canv.lineTo(i * (CANV_WIDTH / appController.numDataPoints), 200 - objectY.perlin[i]);		
 	}
 	canv.stroke();
 	
 	canv.fillStyle = appController.color;
-	for (var i = 0; i <= appController.numDataPoints; i+=1) {
+	for (var i = 0; i <= appController.numDataPoints; i += 1) {
 		canv.beginPath();
-		canv.arc(i*(CANV_WIDTH/appController.numDataPoints), 200-objectY.perlin[i], 2, 2, 360);
+		canv.arc(i * (CANV_WIDTH / appController.numDataPoints), 200 - objectY.perlin[i], 2, 2, 360);
 		canv.fill();
 	}
 }
